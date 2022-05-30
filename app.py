e9964e445eada9cd525a614a22f3700470cadbdd
Felix Ivander <felix.ganumba@gmail.com>
2022-05-30 14:37:15 +0700

    Setting Up Flask Server

--- app.py
+++ app.py
@@ -2,8 +2,4 @@ from flask import Flask, request
 import tensorflow as tf
 import numpy as np
 
-model = tf.keras.models.load_model('./toxicity_classifier/model/model_20220525-235635')
-# input_text = vectorizer('bego lu anjing')
-# res = model.predict(np.expand_dims(input_text,0))
-# print((res > 0.5).astype(int))
-# print(res.shape)
+# model = tf.keras.models.load_model('/toxicity_classifier\logs\fit\20220526-172211')
