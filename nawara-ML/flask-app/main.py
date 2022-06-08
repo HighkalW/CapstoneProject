65f2f0ada0dd0bd72e9a114851b70cc82d5b51c2
Felix Ivander <felix.ganumba@gmail.com>
2022-06-08 22:07:44 +0700

    Fixing Flask App to load the correct model and moving Flask app for AI Platform into folder

--- nawara-ML/flask-app/main.py
+++ nawara-ML/flask-app/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import tensorflow as tf
 import numpy as np
 
-model_path = 'nawara-ML/toxicity_classifier/model/model_20220526-181030'
+model_path = 'nawara-ML/toxicity_classifier/model/model_20220608-214838'
 model = tf.keras.models.load_model(model_path)
 
 app = Flask(__name__)
@@ -11,19 +11,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Nawara!'
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     data = data["comment"]
     prediction = model.predict(np.expand_dims(data, 0)).astype(int)
     return str(prediction[0,0])
-
-# @app.route('/create_table', methods=['POST'])
-# def add_to_database():
-#     data = request.get_json()
-#     #create engine sqlalchemy.c
-#     #establish connection
 
 if __name__ == "__main__":
     app.run()
--- nawara-ML/flask-app/main_test.py
+++ nawara-ML/flask-app/main_test.py
@@ -2,11 +2,10 @@ from flask import Flask, request
 import tensorflow as tf
 import numpy as np
 
-model_path = 'nawara-ML/toxicity_classifier/model/model_20220526-181030/'
+model_path = 'nawara-ML/toxicity_classifier/model/model_20220608-214838/'
 model = tf.keras.models.load_model(model_path)
 
 print(model.summary())
 
-
-# data = "apa sih"
-# print(model.predict(np.expand_dims(data, 0)))
+data = "apa sih bodoh"
+print(model.predict(np.expand_dims(data, 0)))
