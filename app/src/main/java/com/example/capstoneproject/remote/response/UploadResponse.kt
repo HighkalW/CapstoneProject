2a505bc97fe4010cce45406c653bcf0fba7f57b4
disastah! <highkal.wahyu7@gmail.com>
2022-06-09 20:23:00 +0700

    add upload response

---
+++ app/src/main/java/com/example/capstoneproject/remote/response/UploadResponse.kt
@@ -0,0 +1,11 @@
+package com.example.capstoneproject.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class UploadResponse (
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String
+)
