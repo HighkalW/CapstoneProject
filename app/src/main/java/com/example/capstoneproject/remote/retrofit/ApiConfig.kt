d70785f060096ea2c46e7e64b4ddc3ee4be6937a
disastah! <highkal.wahyu7@gmail.com>
2022-06-09 19:58:57 +0700

    add ApiConfig object using BASE_URL = "http://api.nawara.com/v1/"

---
+++ app/src/main/java/com/example/capstoneproject/remote/retrofit/ApiConfig.kt
@@ -0,0 +1,28 @@
+package com.example.capstoneproject.remote.retrofit
+
+import androidx.viewbinding.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiConfig {
+    var BASE_URL = "http://api.nawara.com/v1/"
+
+    fun getApiService(): ApiService {
+        val loggingInterceptor = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
