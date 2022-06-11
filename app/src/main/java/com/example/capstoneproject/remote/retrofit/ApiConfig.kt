ab945f79392390c60c4211c5ba47a935ffbac1ac
disastah! <highkal.wahyu7@gmail.com>
2022-06-11 18:20:15 +0700

    testing to connect with api

--- app/src/main/java/com/example/capstoneproject/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/example/capstoneproject/remote/retrofit/ApiConfig.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    var BASE_URL = "http://api.nawara.com/v1/"
+    var BASE_URL = "https://story-api.dicoding.dev/v1/"
 
     fun getApiService(): ApiService {
         val loggingInterceptor = if (BuildConfig.DEBUG) {
