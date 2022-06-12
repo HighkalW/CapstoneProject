69d6f08b82743a27958060da94f019f646c8c05c
disastah! <highkal.wahyu7@gmail.com>
2022-06-12 21:14:14 +0700

    generate upload response from JSON

--- app/src/main/java/com/example/capstoneproject/remote/response/UploadResponse.kt
+++ app/src/main/java/com/example/capstoneproject/remote/response/UploadResponse.kt
@@ -2,10 +2,35 @@ package com.example.capstoneproject.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class UploadResponse (
-    @field:SerializedName("error")
-    val error: Boolean,
+data class UploadResponse(
 
-    @field:SerializedName("message")
-    val message: String
+	@field:SerializedName("data")
+	val data: Data? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
+
+data class Data(
+
+	@field:SerializedName("image")
+	val image: String? = null,
+
+	@field:SerializedName("createdAt")
+	val createdAt: String? = null,
+
+	@field:SerializedName("__v")
+	val V: Int? = null,
+
+	@field:SerializedName("_id")
+	val id: String? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("desc")
+	val desc: String? = null,
+
+	@field:SerializedName("updatedAt")
+	val updatedAt: String? = null
 )
