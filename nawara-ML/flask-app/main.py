8ce2cd54c69a63e74132664d61bd473565f02f65
Felix Ivander <felix.ganumba@gmail.com>
2022-06-12 14:58:12 +0700

    fixed flask app to return JSON as response

--- nawara-ML/flask-app/main.py
+++ nawara-ML/flask-app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import tensorflow as tf
 import numpy as np
+import json
 
 model_path = 'nawara-ML/toxicity_classifier/model/model_20220608-214838'
 model = tf.keras.models.load_model(model_path)
@@ -16,7 +17,16 @@ def predict():
     data = request.get_json()
     data = data["comment"]
     prediction = model.predict(np.expand_dims(data, 0)).astype(int)
-    return str(prediction[0,0])
+    # return str(prediction[0,0]) # for string response
+    return send_json(prediction)
+
+
+def send_json(prediction):
+    response_json = {
+        "code": 200,
+        "prediction": int(prediction[0,0])
+    }
+    return json.dumps(response_json)
 
 if __name__ == "__main__":
     app.run()
