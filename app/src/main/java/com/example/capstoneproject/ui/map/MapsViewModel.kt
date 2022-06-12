bae3dab99034463614261584790e42e8109ebc83
disastah! <highkal.wahyu7@gmail.com>
2022-06-12 21:11:21 +0700

    deleted map

--- app/src/main/java/com/example/capstoneproject/ui/map/MapsViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.capstoneproject.ui.map
-
-import androidx.lifecycle.ViewModel
-import com.example.capstoneproject.data.repo.StoryRepo
-
-class MapsViewModel(private val storyRepo: StoryRepo) : ViewModel() {
-    fun getStories(token: String) = storyRepo.getStoryLocation(token)
-}
